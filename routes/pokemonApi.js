281bc270508de0938bde8adb21ec456f5aeea272
matthewjstahnke <matthewjstahnke@gmail.com>
2022-03-21 13:17:45 -0400

    imported pokeapi

--- routes/pokemonApi.js
+++ routes/pokemonApi.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router=express.Router();
+// var express = require('express');
+// var router=express.Router();
 
-router.get('/',function(req, res, next){
-  res.send('API is working')
-})
+// router.get('/',function(req, res, next){
+//   res.send('API is working')
+// })
 
-module.exports=router;
+// module.exports=router;
+import Pokedex from 'pokedex-promise-v2';
+const P = new Pokedex();
